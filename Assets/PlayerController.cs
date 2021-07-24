72a9fe590c8a93c7030dd672ee2123d53974fc99
agnieszka-tomasik <agnieszka_tomasik@Kaczuszka.localdomain>
2021-07-24 15:38:03 -0400

    Enhanced player movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -16,18 +16,18 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
 
-            rb.velocity = new Vector2(-5, rb.velocity.y);
+            rb.velocity = new Vector2(-10, rb.velocity.y);
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
 
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
 
-            rb.velocity = new Vector2(5, rb.velocity.y);
+            rb.velocity = new Vector2(10, rb.velocity.y);
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
 
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
         {
 
             rb.velocity = new Vector2(rb.velocity.x, 10);
