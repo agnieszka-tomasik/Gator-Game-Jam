dd2c969e2a01231e688b7d9a7a4df5b7b463d4e5
Morgan Askeland <61073367+AskelandL@users.noreply.github.com>
2021-07-25 07:19:31 -0400

    Textmesh files and adding to MainScreen

---
+++ Assets/Door.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public PlayerController player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetRelicCount >= 3)
+        {
+            // open door
+            Debug.Log("Door Open");
+            Destroy(gameObject, 1);
+        }
+    }
+}
