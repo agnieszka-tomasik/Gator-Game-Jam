2a7a1726dff1c8146436e86063e2c34f6c879ff2
Morgan Askeland <61073367+AskelandL@users.noreply.github.com>
2021-07-25 09:44:45 -0400

    halfway working text

---
+++ Assets/Text.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Text : MonoBehaviour
+{
+    public string text;
+    public Text m_myText;
+    bool seenDoor;
+
+    void Start()
+    {
+        //Text sets your text to say this message
+        m_myText.text = "WASD or arrows to walk\nE to stun\nQ to capture";
+        seenDoor = false;
+    }
+
+    void Update()
+    {
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.name == "PapaWiz")
+            {
+                m_myText.text = "Please help me open this door.\n You will need to collect three artifacts to get inside.";
+            }
+            if (other.gameObject.name == "Barrier" && !seenDoor)
+            {
+                seenDoor = true;
+                m_myText.text = "Open smaller doors by collecting monsters.\n Use E to stun, then Q to capture.";
+            }
+        }
+    }
+}
